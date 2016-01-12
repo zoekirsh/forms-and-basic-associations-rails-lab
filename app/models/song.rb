e5853f85a582c25bfb36a31525433f6630e7edbb
Ashi Krishnan <me@ashi.works>
2016-01-12 13:21:17 -0500

    Relations and bulk setter methods.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      self.notes.build(content: content)
+    end
+  end
 end
